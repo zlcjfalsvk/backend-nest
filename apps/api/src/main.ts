fbdf6d93340e74ad198dabf017f4335edcc1b655
charlie.choi <zl6863vk@gmail.com>
2025-06-10 16:51:00 +0900

    chore: monorepo init

---
+++ apps/api/src/main.ts
@@ -0,0 +1,8 @@
+import { NestFactory } from '@nestjs/core';
+import { ApiModule } from './api.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(ApiModule);
+  await app.listen(process.env.port ?? 3000);
+}
+bootstrap();
--- apps/api/test/app.e2e-spec.ts
+++ apps/api/test/app.e2e-spec.ts
@@ -1,15 +1,14 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
-import { App } from 'supertest/types';
-import { AppModule } from './../src/app.module';
+import { ApiModule } from './../src/api.module';
 
-describe('AppController (e2e)', () => {
-  let app: INestApplication<App>;
+describe('ApiController (e2e)', () => {
+  let app: INestApplication;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
+      imports: [ApiModule],
     }).compile();
 
     app = moduleFixture.createNestApplication();
